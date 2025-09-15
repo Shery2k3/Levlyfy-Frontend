7f39e7667774b9415b77b7bcf1f876188cfeaac5
Shahryar Khan <shahryar2k3@gmail.com>
2025-09-15 07:25:02 +0500

    token issue

--- lib/api.ts
+++ lib/api.ts
@@ -22,4 +22,19 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+//? Auto handle 401 errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        window.location.href = "/auth/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
